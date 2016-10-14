80347db5226ec12b46a15e4e810636587d766038
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2016-10-14 16:36:13 +0300

    Add do-while, for, variables, not working yet...

--- LineBreaker.java
+++ LineBreaker.java
@@ -9,20 +9,44 @@
 
 public class LineBreaker {
     public static void main(String[] args) {
+        final int MINTEKSTINLEVEYS = 3;
+
         System.out.println("Hello! I break lines.");
 
         // Jos uudestaan true,
-        boolean uudestaan = true;
+        boolean syoteOk = false;
+        String tekstisyote = "";
+        char valilyonti = ' ';
+        int tekstisyotteenPituus = tekstisyote.length();
+        int sananPituus = tekstisyotteenPituus + 1;
+        int pisinSananPituus = tekstisyotteenPituus + 1;
 
-        // Pyydetään käyttäjää syöttämään tekstialueen leveys.
-        System.out.println("Enter area width:");
-        // Luetaan tekstialueen leveys käyttäjältä.
-        int tekstialueenLeveys = In.readInt();
-        if (tekstialueenLeveys >= 3) {
-            System.out.println("Enter a line:");
-            String tekstirivi = In.readString();
-        } else {
-            System.out.println("Error!");
-        }
+        do {
+            // Pyydetään käyttäjää syöttämään tekstialueen leveys.
+            System.out.println("Enter area width:");
+            // Luetaan tekstialueen leveys käyttäjältä.
+            int tekstialueenLeveys = In.readInt();
+            // Jos tekstialueen leveys on suurempi tai yhtä suuri kuin
+            // määritelty minimileveys...
+            if (tekstialueenLeveys >= MINTEKSTINLEVEYS) {
+                do {
+                    System.out.println("Enter a line:");
+                    syoteOk = true;
+                    tekstisyote = In.readString();
+
+                        for (int i = 0; i < tekstisyotteenPituus; i++) {
+                            if (i == tekstisyotteenPituus || tekstisyote.charAt(i) == valilyonti) {
+                                if (sananPituus < pisinSananPituus) {
+                                    pisinSananPituus = sananPituus;
+                                }
+                            }
+                            sananPituus++;
+                        }
+
+                } while (pisinSananPituus > tekstialueenLeveys);
+            } else {
+                System.out.println("Error!");
+            }
+        } while (!syoteOk);
     }
 }
