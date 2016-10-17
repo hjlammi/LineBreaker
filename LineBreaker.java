7be6fd2a27dd83ec8694db2785b1a0b79cc6d3c4
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2016-10-17 18:04:17 +0300

    Add loops for word length and continue

--- LineBreaker.java
+++ LineBreaker.java
@@ -3,13 +3,15 @@
  *
  * Heidi Lammi-Mihaljov, Lammi-Mihaljov.Heidi.J@student.uta.fi
  *
- * Viimeksi muokattu: 13.10.2016.
+ * Viimeksi muokattu: 17.10.2016.
  *
  */
 
 public class LineBreaker {
     public static void main(String[] args) {
         final int MINTEKSTINLEVEYS = 3;
+        final char YES = 'y';
+        final char NO = 'n';
 
         System.out.println("Hello! I break lines.");
 
@@ -21,40 +23,55 @@ public class LineBreaker {
         int sananPituus = 0;
         int pisinSananPituus;
         boolean liianPitkaSana = true;
+        char vastaus;
+        boolean vastausOK = false;
 
         do {
-            // Pyydetään käyttäjää syöttämään tekstialueen leveys.
-            System.out.println("Enter area width:");
-            // Luetaan tekstialueen leveys käyttäjältä.
-            tekstialueenLeveys = In.readInt();
-            // Jos tekstialueen leveys on suurempi tai yhtä suuri kuin
-            // määritelty minimileveys...
-            if (tekstialueenLeveys >= MINTEKSTINLEVEYS) {
-                do {
-                    pisinSananPituus = 0;
-                    System.out.println("Enter a line:");
-                    syoteOk = true;
-                    tekstisyote = In.readString();
-                    tekstisyotteenPituus = tekstisyote.length();
+            do {
+                // Pyydetään käyttäjää syöttämään tekstialueen leveys.
+                System.out.println("Enter area width:");
+                // Luetaan tekstialueen leveys käyttäjältä.
+                tekstialueenLeveys = In.readInt();
+                // Jos tekstialueen leveys on suurempi tai yhtä suuri kuin
+                // määritelty minimileveys...
+                if (tekstialueenLeveys >= MINTEKSTINLEVEYS) {
+                    do {
+                        pisinSananPituus = 0;
+                        System.out.println("Enter a line:");
+                        syoteOk = true;
+                        tekstisyote = In.readString();
+                        tekstisyotteenPituus = tekstisyote.length();
 
-                    for (int i = 0; i <= tekstisyotteenPituus; i++) {
-                        liianPitkaSana = pisinSananPituus > tekstialueenLeveys;
-                        if (i == tekstisyotteenPituus || tekstisyote.charAt(i) == valilyonti) {
-                            if (sananPituus > pisinSananPituus) {
-                                pisinSananPituus = sananPituus;
+                        for (int i = 0; i <= tekstisyotteenPituus; i++) {
+                            liianPitkaSana = pisinSananPituus > tekstialueenLeveys;
+                            if (i == tekstisyotteenPituus || tekstisyote.charAt(i) == valilyonti) {
+                                if (sananPituus > pisinSananPituus) {
+                                    pisinSananPituus = sananPituus;
+                                }
+                                sananPituus = 0;
+                            } else {
+                                sananPituus++;
                             }
-                            sananPituus = 0;
-                        } else {
-                            sananPituus++;
                         }
+                        if (liianPitkaSana) {
+                            System.out.println("Error!");
+                        }
+                    } while (liianPitkaSana);
+                } else {
+                    System.out.println("Error!");
+                }
+            } while (!syoteOk);
+            // Kysytään käyttäjältä, jatketaanko merkkijonojen rivittämistä.
+            System.out.println("Continue (y/n)?");
+            // Luetaan vastaus
+            vastaus = In.readChar();
+            do {
+                vastausOK = ((vastaus == YES) || (vastaus == NO));
+                if (vastaus == NO) {
+                    System.out.println("See you soon.");
                     }
-                    if (liianPitkaSana) {
-                        System.out.println("Error!");
-                    }
-                } while (liianPitkaSana);
-            } else {
-                System.out.println("Error!");
-            }
-        } while (!syoteOk);
+            } while (!vastausOK);
+        } while (vastaus == YES);
+
     }
 }
