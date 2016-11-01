1aec9f27ff709705d78308996daa2a73d6597ea4
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2016-11-01 15:02:15 +0200

    Add viimIndeksiTokanRivinLopussa

--- LineBreaker4.java
+++ LineBreaker4.java
@@ -1,10 +1,12 @@
 public class LineBreaker4 {
-    public static int etsiRivinViimTulostettavaIndeksi(String syote, char erotin, int rivinPituus) {
+    public static int etsiRivinViimTulostettavaIndeksi(String syote, char erotin, int rivinPituus, int rivinEnsimmainenIndeksi) {
         boolean erotinLoytyi = false;
         int tutkittavaIndeksi = 0;
         int rivinViimTulostettavaIndeksi = 0;
         int rivinViimTutkittavaIndeksi = 8;
 
+        tutkittavaIndeksi = rivinEnsimmainenIndeksi;
+        rivinViimTutkittavaIndeksi = rivinEnsimmainenIndeksi + rivinPituus;
         do {
             if (syote.charAt(tutkittavaIndeksi) == erotin) {
                 rivinViimTulostettavaIndeksi = tutkittavaIndeksi - 1;
--- test/LineBreakerTest.java
+++ test/LineBreakerTest.java
@@ -6,15 +6,21 @@ public class LineBreakerTest {
 
     @Test
     public void viimIndeksiEkanRivinLopussa() {
-        assertEquals(7, LineBreaker4.etsiRivinViimTulostettavaIndeksi("Testing, testing. One, two, three.", ' ', 8));
+        assertEquals(7, LineBreaker4.etsiRivinViimTulostettavaIndeksi("Testing, testing. One, two, three.", ' ', 8, 0));
     }
 
     @Test
     public void viimIndeksiEnnenEkanRivinLopussa() {
-        assertEquals(4, LineBreaker4.etsiRivinViimTulostettavaIndeksi("Test, test. One, two, three.", ' ', 8));
+        assertEquals(4, LineBreaker4.etsiRivinViimTulostettavaIndeksi("Test, test. One, two, three.", ' ', 8, 0));
     }
+
     @Test
     public void viimIndeksiMontaSanaaEkallaRivilla() {
-        assertEquals(6, LineBreaker4.etsiRivinViimTulostettavaIndeksi("One two three four five.", ' ', 8));
+        assertEquals(6, LineBreaker4.etsiRivinViimTulostettavaIndeksi("One two three four five.", ' ', 8, 0));
+    }
+
+    @Test
+    public void viimIndeksiTokanRivinLopussa() {
+        assertEquals(16, LineBreaker4.etsiRivinViimTulostettavaIndeksi("Testing, testing. One, two, three.", ' ', 8, 9));
     }
 }
